4258dbc332c8713e6ab5bd28c558db351c27c236
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-06-28 02:09:30 +0400

    Update deepface_r.py

--- source/deepface_r.py
+++ source/deepface_r.py
@@ -1,2 +1,9 @@
 # pip install deepface
 # pip install pyttsx3
+
+# facial recognition library
+from deepface import DeepFace 
+# python speech library
+import pyttsx3
+# to plot the image
+import matplotlib.pyplot as plt

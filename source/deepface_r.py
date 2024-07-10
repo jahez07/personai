53302e130931bace0420f70b70ca6c6234814f29
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-07-10 06:30:03 +0400

    Update deepface_r.py

--- source/deepface_r.py
+++ source/deepface_r.py
@@ -1,10 +1,27 @@
-# pip install deepface
-# pip install pyttsx3
-
-# facial recognition library
-from deepface import DeepFace 
-# python speech library
-import pyttsx3
-# to plot the image
+from flask import Flask, render_template, request, redirect
+import pandas as pd
+#import tensorflow as tf
 import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+import numpy as np
 
+#model = tf.keras.models.load_model('/Users/jahezabrahamjohny/Documents/GitHub/CoreAxon/autoencoder_model.h5')
+
+
+app = Flask(__name__)
+
+@app.route('/pi', methods=['GET'])
+def hello_word():
+    return render_template('index.html')
+
+@app.route('/pi', methods=['POST'])
+def predict():
+    file= request.files['image']
+    
+    
+            # Render the template with prediction results and plot
+    return
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)

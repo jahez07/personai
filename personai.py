f3c528a11e33adbe27816ae107bab5dacb09df54
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-07-12 15:49:14 +0400

    Update personai.py

--- personai.py
+++ personai.py
@@ -37,3 +37,22 @@ generation_config = {
     "top_k": 1,
     "max_output_tokens":2048,
 }
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE"
+    },
+]
